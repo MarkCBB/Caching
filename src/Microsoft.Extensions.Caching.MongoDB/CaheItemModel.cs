efce90dbdd83f5f282dc287d53b095860f69f042
MarkCBB <mark@markcortada.com>
2018-04-29 13:51:17 +0200

    Created initial CaheItemModel to store cache data in the database.

---
+++ src/Microsoft.Extensions.Caching.MongoDB/CaheItemModel.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Caching.MongoDB
+{
+    [BsonIgnoreExtraElements]
+    public class CaheItemModel
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
