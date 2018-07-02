393e24fa931a283bbfa0b8382b215d5b2a3cd75d
MarkCBB <mark@markcortada.com>
2018-07-02 13:45:10 +0200

    Moved index test cases in classes

---
+++ test/Microsoft.Extensions.Caching.MongoDB.Tests/MongoDBCacheCreateIndexInDbTests3.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.Extensions.Caching.MongoDB
+{
+    public class MongoDBCacheCreateIndexInDbTests3 : BaseMongoDbTests
+    {
+        [Fact(Skip = SkipReason)]
+        public void MongoDBCache_DoNotCreateAnyIndexDetailedInParameters()
+        {
+            // Arrange
+            var databaseName = "DoNotCreateAnyIndex_" + GetRandomNumber();
+            var mongoOptions = new MongoDBCacheOptions()
+            {
+                ConnectionString = "mongodb://mongo1:27018,mongo1:27019,mongo1:27020",
+                DatabaseName = databaseName,
+                CollectionName = "CacheCollectionTests",
+                CreateCoverIndex = false,
+                CreateTTLIndex = false,
+                CreateIndexesInBackground = false
+            };
+            // We must create a new mongoDB cache in every test becasue this parameters and
+            // the index creation are set only once
+            var cache = new MongoDBCache(mongoOptions);
+            var key = GetRandomNumber();
+            var value = Encoding.ASCII.GetBytes("MongoDBCache_CreateIndexIfIsDetailedInParameters");
+            var cacheOptions = new Distributed.DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.UtcNow
+            };
+
+            // Act
+            cache.Set(key, value, cacheOptions);
+
+            // Assert
+            var indexList = cache.GetCollection().Indexes.List().ToList();
+            Assert.False(ExistsIndexInListHelper(indexList, "fullItemIndex"));
+            Assert.False(ExistsIndexInListHelper(indexList, "TTLItemIndex"));
+        }
+    }
+}
